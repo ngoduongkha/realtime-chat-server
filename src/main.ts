97cf414b37a26f19a142529e26356801e81b1e4b
Ngo Duong Kha <ngoduongkhakg2001@gmail.com>
2022-11-27 15:08:22 +0700

    fix: deploy

--- src/main.ts
+++ src/main.ts
@@ -48,7 +48,7 @@ async function bootstrap(): Promise<void> {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(80);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
